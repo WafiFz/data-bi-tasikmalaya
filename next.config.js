d1302aa996b2dbb33c0f4587e9b0d677e2e6d558
WafiFz <wafifahruz@gmail.com>
2024-03-06 09:33:14 +0700

    chore(next.config.js): add support for loading environment variables from a .env file to improve configuration flexibility chore(package.json): add dotenv package as a dependency to enable loading environment variables from a .env file

--- next.config.js
+++ next.config.js
@@ -1,5 +1,14 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const path = require('path')
+const path = require('path');
+const dotenv = require('dotenv');
+const fs = require('fs');
+
+// Read ENV
+const envFile = path.resolve(__dirname, '.env');
+const envConfig = fs.existsSync(envFile) ? require('dotenv').parse(fs.readFileSync(envFile)) : {};
+
+// Load environment variables from .env file
+dotenv.config();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -14,7 +23,10 @@ const nextConfig = {
         apexcharts: path.resolve(__dirname, './node_modules/apexcharts-clevision'),
       },
     },
-  })
-}
+  }),
+  env: {
+    ...envConfig,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
