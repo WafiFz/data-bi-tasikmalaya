81f3c0b99286cefdf15b5b2dafbbf5e435859d04
WafiFz <wafifahruz@gmail.com>
2024-03-18 13:01:28 +0700

    feat: add new image for BI Tasikmalaya building
    feat: add SearchBox component for BI Tasikmalaya input
    
    feat: add translation for search box placeholder in Indonesian language
    
    refactor: update HeroSection component to use SearchBox component
    
    refactor: update styling for code blocks and add custom blue color
    
    docs: update README with new changes

---
+++ src/@core/components/bi-tasik/input/SearchBox.tsx
@@ -0,0 +1,31 @@
+import SearchIcon from '@mui/icons-material/Search'
+import IconButton from '@mui/material/IconButton'
+import InputBase from '@mui/material/InputBase'
+import Paper from '@mui/material/Paper'
+import { FormattedMessage, useIntl } from 'react-intl'
+
+export default function SearchBox() {
+  const intl = useIntl()
+  const placeholderSearchBox = intl.formatMessage(
+    {
+      id: 'placeholder.searchbox'
+    },
+    { defaultMessage: 'Search' }
+  )
+
+  return (
+    <Paper
+      component="form"
+      sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: 400 }}
+    >
+      <InputBase
+        sx={{ ml: 1, flex: 1 }}
+        placeholder={placeholderSearchBox}
+        inputProps={{ 'aria-label': 'search' }}
+      />
+      <IconButton type="button" sx={{ p: '10px' }} aria-label="search">
+        <SearchIcon />
+      </IconButton>
+    </Paper>
+  )
+}
--- src/views/home/HeroSection.tsx
+++ src/views/home/HeroSection.tsx
@@ -1,34 +1,34 @@
-import React from 'react';
+import SearchBox from '@core/components/bi-tasik/input/SearchBox'
+import React from 'react'
 
 const HeroSection: React.FC = () => {
   return (
-    <div className="sc-fodVek fBnKBn flex flex-col md:flex-row">
+    <div className="flex flex-col justify-center items-center md:flex-row">
       <div className="sc-fFucqa gxikOp">
-        <div>
-          <p className="sc-bkzYnD lcvJDI">Akses Data Ind<span className="sc-bkzYnD lcvJDI" style={{ color: 'red' }}>onesia</span></p>
-          <p className="sc-bkzYnD lcvJDI">dalam<span style={{ color: 'red' }}> Satu Portal</span></p>
-          <div className="sc-dIUeWJ kbqGlW flex">
-            <p className="sc-idOiZg gpvpej" style={{ color: 'red', marginRight: '10px', fontWeight: 'bold' }}>363804</p>
-            <p className="sc-idOiZg gpvpej">DATASET TERSEDIA</p>
+        <div className="text-2xl font-bold">
+          <p>DATA</p>
+          <p className="text-4xl">
+            BI Kpw <span className="text-blue">TASIKMALAYA</span>
+          </p>
+          {/* <p>
+              Dalam&nbsp;
+              <span className="text-blue">Satu Portal</span>
+            </p> */}
+
+          <div className="mt-8 flex">
+            <p className="text-blue mr-4">363804</p>
+            <p>DATASET TERSEDIA</p>
           </div>
         </div>
-        <div className="sc-kstqJO hxBIOc mt-4 md:mt-0 md:ml-4 flex items-center">
-          <div className="basic-single grouped-dropdownNew md:d-none d-md-block css-b62m3t-container flex-grow">
-            {/* Dropdown component */}
-          </div>
-          <div className="sc-hBEYId jUZYmd md:ml-4 mt-4 md:mt-0">
-            <input name="searchText" placeholder="Cari Data Publik Indonesia" value="" className="border-gray-300 border-2 rounded-md py-2 px-4 w-full focus:outline-none focus:border-blue-500" />
-            <div className="btn-search svg-15 ml-2">
-              {/* Search button icon */}
-            </div>
-          </div>
+        <div className="mt-4">
+          <SearchBox></SearchBox>
         </div>
       </div>
-      <div style={{ marginLeft: '20px', width: '30%' }}>
-        <img className="sc-hHfuMS dwLdMQ" src="https://data.go.id/api-be/file/public-view/indonesia_20231012060306_20231013151237.png" />
+      <div className="hidden ml-32 md:flex" style={{ width: '40%' }}>
+        <img className="" src="images/bi/gedung-bi-tasik.png" />
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default HeroSection;
+export default HeroSection
