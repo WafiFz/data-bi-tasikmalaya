9b2f64b40796e803ca17b31c85b1c7a50db4ba93
WafiFz <wafifahruz@gmail.com>
2024-03-22 08:39:00 +0700

    feat: rename Dashboard to IndexPage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import DataTopics from '@views/home/DataTopics'
 import HeroSection from '@views/home/HeroSection'
 import ListFileSection from '@views/home/ListFileSection'
 
-export default function Dashboard() {
+export default function IndexPage() {
   if (typeof window === 'undefined') {
     console.log('window is undefined')
 
